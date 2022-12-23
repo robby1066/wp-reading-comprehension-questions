00010d1b52c6e7c88a2f752b7145e04210bbd0e0
robby1066 <robbymacdonell@gmail.com>
2022-12-23 15:40:59 -0800

    first commit

---
+++ reading-comprehension-questions.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Reading Comprehension Questions
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     reading-comprehension-questions
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Reading_Comprehension_Questions
+ */
+
+// Your code starts here.
---
+++ tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Reading_Comprehension_Questions
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
